63f13a0a7dccdf48ebba3235de4608ff2b23b296
Jaideep P <jaideepp.xworkz@gmail.com>
2022-09-13 17:18:31 +0530

    parameter

---
+++ Identity.java
@@ -0,0 +1,12 @@
+class Identity{
+	static void printName(String name)
+	{
+		System.out.println("Name entered :"+name);
+	}
+	static void printnameandemail(String name, String email)
+	{
+		System.out.println("Name and Email ID entered : "+name+email); 
+
+	}
+	
+}
---
+++ IdentityRunner.java
@@ -0,0 +1,25 @@
+class IdentityRunner{
+	public static void main(String...identity)
+	{
+		
+	String value="Jaideep";
+        Identity.printName(value);
+		Identity.printName("Jagadeeshallagi");
+		Identity.printName("Omkar");
+		Identity.printName("Prashanth");
+		Identity.printName("Rudresh");
+		Identity.printName("Vinay");
+		Identity.printName("shilpa");
+		Identity.printName("Aishwarya");
+		Identity.printName("Anusha");
+		Identity.printName("Veena");
+		Identity.printName("Sachin");
+        Identity.printName("Sharath");
+		String name="Omkar";
+		String email="Omkar.xworkz@gmail.com";
+		Identity.printnameandemail(name,email);
+		
+		
+		}
+}
+        
