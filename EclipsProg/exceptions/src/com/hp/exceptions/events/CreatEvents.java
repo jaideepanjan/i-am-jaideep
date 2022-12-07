331add461f31a0703be0ce61a18a735b2732dfaa
Jaideep P <jaideepp.xworkz@gmail.com>
2022-12-07 08:22:54 +0530

    events

---
+++ EclipsProg/exceptions/src/com/hp/exceptions/events/CreatEvents.java
@@ -0,0 +1,491 @@
+package com.hp.exceptions.events;
+
+import java.awt.AWTError;
+import java.awt.AWTException;
+import java.awt.FontFormatException;
+import java.awt.color.CMMException;
+import java.awt.color.ProfileDataException;
+import java.awt.datatransfer.MimeTypeParseException;
+import java.awt.geom.IllegalPathStateException;
+import java.awt.geom.NoninvertibleTransformException;
+import java.awt.image.ImagingOpException;
+import java.awt.image.RasterFormatException;
+import java.io.IOError;
+import java.io.IOException;
+import java.lang.annotation.AnnotationTypeMismatchException;
+import java.lang.annotation.IncompleteAnnotationException;
+import java.lang.instrument.IllegalClassFormatException;
+import java.lang.invoke.WrongMethodTypeException;
+import java.lang.reflect.GenericSignatureFormatError;
+import java.lang.reflect.UndeclaredThrowableException;
+import java.nio.BufferOverflowException;
+import java.nio.channels.AlreadyBoundException;
+import java.nio.charset.CoderMalfunctionError;
+import java.nio.file.FileSystemNotFoundException;
+import java.nio.file.ProviderNotFoundException;
+import java.rmi.activation.ActivationException;
+import java.security.GeneralSecurityException;
+import java.security.ProviderException;
+import java.security.acl.AclNotFoundException;
+import java.security.acl.LastOwnerException;
+import java.security.cert.CertificateException;
+import java.util.ConcurrentModificationException;
+import java.util.IllformedLocaleException;
+import java.util.MissingResourceException;
+import java.util.NoSuchElementException;
+import java.util.ServiceConfigurationError;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.zip.DataFormatException;
+
+import javax.lang.model.UnknownEntityException;
+import javax.management.BadAttributeValueExpException;
+import javax.management.BadBinaryOpValueExpException;
+import javax.management.BadStringOperationException;
+import javax.management.IntrospectionException;
+import javax.management.InvalidApplicationException;
+import javax.management.JMException;
+import javax.management.JMRuntimeException;
+import javax.management.RuntimeErrorException;
+import javax.management.modelmbean.InvalidTargetObjectTypeException;
+import javax.naming.NamingException;
+import javax.print.attribute.UnmodifiableSetException;
+import javax.security.auth.DestroyFailedException;
+import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.MidiUnavailableException;
+import javax.sound.sampled.LineUnavailableException;
+import javax.swing.text.BadLocationException;
+import javax.swing.tree.ExpandVetoException;
+import javax.swing.undo.CannotRedoException;
+import javax.swing.undo.CannotUndoException;
+import javax.xml.bind.DataBindingException;
+import javax.xml.bind.TypeConstraintException;
+import javax.xml.crypto.KeySelectorException;
+import javax.xml.crypto.MarshalException;
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.ws.WebServiceException;
+
+import org.ietf.jgss.GSSException;
+import org.omg.CORBA.SystemException;
+import org.omg.CORBA.portable.ApplicationException;
+import org.w3c.dom.DOMException;
+import org.w3c.dom.ls.LSException;
+
+public class CreatEvents {
+	
+		
+	
+	public void joy() {
+		System.out.println("Running the exception method 1");
+		throw new RuntimeErrorException(null);
+	}
+	public void jaideep() {
+		System.out.println("Running the exception method 2");
+		throw new AnnotationTypeMismatchException(null, null);
+	}
+	public void jaggi() {
+		System.out.println("Running the exception method 3");
+		throw new  ArithmeticException();
+	}
+	public void jagadeesh() {
+		System.out.println("Running the exception method 4");
+		throw new  ArrayStoreException();
+	}
+	public void allagi() {
+		System.out.println("Running the exception method 5");
+		throw new BufferOverflowException();
+		}
+	public void anju() {
+		System.out.println("Running the exception method 6");
+		throw new  CannotRedoException();
+	}
+	public void amul() {
+		System.out.println("Running the exception method 7");
+		throw new CannotUndoException();
+	}
+	public void jaggu() {
+		System.out.println("Running the exception method 8");
+		throw new  ClassCastException(null);
+	}
+	public void appu() {
+		System.out.println("Running the exception method 9");
+		throw new  CMMException(null);
+	}
+	public void ammu() {
+		System.out.println("Running the exception method 10");
+		throw new DataBindingException(null);
+	}
+	public void muddu() {
+		System.out.println("Running the exception method 11");
+		throw new  DOMException((short) 0, null);
+	}
+	public void bangari() {
+		System.out.println("Running the exception method 12");
+		throw new  FileSystemNotFoundException(null);
+	}
+	public void captain() {
+		System.out.println("Running the exception method 13");
+		throw new IllegalMonitorStateException(null);
+	}
+	public void power() {
+		System.out.println("Running the exception method 14");
+		throw new IllegalPathStateException(null);
+	}
+	public void jeevs() {
+		System.out.println("Running the exception method 15");
+		throw new  IllformedLocaleException(null);
+	}
+	public void peddu() {
+		System.out.println("Running the exception method 16");
+		throw new ImagingOpException(null);
+	}
+	public void sneha() {
+		System.out.println("Running the exception method 17");
+		throw new IncompleteAnnotationException(null, null);
+	}
+	public void sani() {
+		System.out.println("Running the exception method 18");
+		throw new  IndexOutOfBoundsException(null);
+	}
+	public void uttu() {
+		System.out.println("Running the exception method 19");
+		throw new JMRuntimeException(null);
+	}
+	public void yashu() {
+		System.out.println("Running the exception method 20");
+		throw new   NoSuchElementException(null);
+	}
+	public void bhabi() {
+		System.out.println("Running the exception method 21");
+		throw new NegativeArraySizeException(null);
+	}
+	public void dada() {
+		System.out.println("Running the exception method 22");
+		throw new  MissingResourceException(null, null, null);
+	}
+	public void amma() {
+		System.out.println("Running the exception method 23");
+		throw new NullPointerException(null);
+	}
+	public void appa() {
+		System.out.println("Running the exception method 24");
+		throw new  ProfileDataException(null);
+	}
+	public void breath() {
+		System.out.println("Running the exception method 25");
+		throw new  ProviderNotFoundException(null);
+	}
+	public void viju() {
+		System.out.println("Running the exception method 26");
+		throw new RasterFormatException(null);
+	}
+	public void anna() {
+		System.out.println("Running the exception method 27");
+		throw new  TypeNotPresentException(null, null);
+	}
+	public void chandi() {
+		System.out.println("Running the exception method 28");
+		throw new  UndeclaredThrowableException(null);
+	}
+	public void anarkali() {
+		System.out.println("Running the exception method 29");
+		throw new  UnmodifiableSetException(null);
+	}
+	public void champakali() {
+		System.out.println("Running the exception method 30");
+		throw new  WrongMethodTypeException(null);
+	}
+	public void jamoon() {
+		System.out.println("Running the exception method 31");
+		throw new JMRuntimeException(null);
+	}
+	public void burfee() {
+		System.out.println("Running the exception method 32");
+		throw new CannotUndoException();
+	}
+	public void palav() {
+		System.out.println("Running the exception method 33");
+		throw new RuntimeErrorException(null);
+	}
+	public void dose() {
+		System.out.println("Running the exception method 34");
+		throw new CMMException(null);
+	}
+	public void puliyogare() {
+		System.out.println("Running the exception method 35");
+		throw new ArrayStoreException(null);
+	}
+	public void dosta() {
+		System.out.println("Running the exception method 36");
+		throw new IncompleteAnnotationException(null, null);
+	}
+	
+	public void deepti() throws AclNotFoundException {
+		System.out.println("Running the Runtime Exeption method 1");
+		throw new AclNotFoundException();		
+	}
+	public void chaitra() throws   ActivationException {
+		System.out.println("Running the Runtime Exeption method 2");
+		throw new   ActivationException();		
+	}
+	public void monu() throws  AlreadyBoundException {
+		System.out.println("Running the Runtime Exeption method 3");
+		throw new  AlreadyBoundException();		
+	}
+	public void deeksha() throws   ApplicationException {
+		System.out.println("Running the Runtime Exeption method 4");
+		throw new   ApplicationException(null, null);		
+	}
+	public void akka() throws AWTException {
+		System.out.println("Running the Runtime Exeption method 5");
+		throw new AWTException(null);		
+	}
+	public void pinki() throws  BadAttributeValueExpException {
+		System.out.println("Running the Runtime Exeption method 6");
+		throw new  BadAttributeValueExpException(null);		
+	}
+	public void minki() throws BadBinaryOpValueExpException {
+		System.out.println("Running the Runtime Exeption method 7");
+		throw new BadBinaryOpValueExpException(null);		
+	}
+	public void chinki() throws  BadLocationException {
+		System.out.println("Running the Runtime Exeption method 8");
+		throw new  BadLocationException(null, 0);		
+	}
+	public void ranna() throws  BadStringOperationException {
+		System.out.println("Running the Runtime Exeption method 9");
+		throw new  BadStringOperationException(null);		
+	}
+	public void bangara() throws BrokenBarrierException {
+		System.out.println("Running the Runtime Exeption method 10");
+		throw new BrokenBarrierException();		
+	}
+	public void darling() throws CertificateException {
+		System.out.println("Running the Runtime Exeption method 11");
+		throw new CertificateException();		
+	}
+	public void veenu() throws CloneNotSupportedException {
+		System.out.println("Running the Runtime Exeption method 12");
+		throw new CloneNotSupportedException();		
+	}
+	public void malli() throws DataFormatException {
+		System.out.println("Running the Runtime Exeption method 13");
+		throw new DataFormatException();		
+	}
+	public void layse() throws  DatatypeConfigurationException {
+		System.out.println("Running the Runtime Exeption method 14");
+		throw new  DatatypeConfigurationException();		
+	}
+	public void action() throws DestroyFailedException {
+		System.out.println("Running the Runtime Exeption method 15");
+		throw new DestroyFailedException();		
+	}
+	public void nandu() throws  ExpandVetoException {
+		System.out.println("Running the Runtime Exeption method 16");
+		throw new  ExpandVetoException(null);		
+	}
+	public void manvi() throws  FontFormatException {
+		System.out.println("Running the Runtime Exeption method 17");
+		throw new  FontFormatException(null);		
+	}
+	public void kavi() throws  GeneralSecurityException {
+		System.out.println("Running the Runtime Exeption method 18");
+		throw new  GeneralSecurityException();		
+	}
+	public void kencha() throws GSSException {
+		System.out.println("Running the Runtime Exeption method 19");
+		throw new GSSException(0);		
+	}
+	public void kempi() throws  IllegalClassFormatException {
+		System.out.println("Running the Runtime Exeption method 20");
+		throw new  IllegalClassFormatException();		
+	}
+	public void hampi() throws  InterruptedException {
+		System.out.println("Running the Runtime Exeption method 21");
+		throw new  InterruptedException();		
+	}
+	public void dummi() throws  IntrospectionException {
+		System.out.println("Running the Runtime Exeption method 22");
+		throw new  IntrospectionException();		
+	}
+	public void kaddi() throws InvalidApplicationException {
+		System.out.println("Running the Runtime Exeption method 23");
+		throw new InvalidApplicationException(null);		
+	}
+	public void nagu() throws InvalidMidiDataException {
+		System.out.println("Running the Runtime Exeption method 24");
+		throw new InvalidMidiDataException();		
+	}
+	public void rakshu() throws InvalidTargetObjectTypeException {
+		System.out.println("Running the Runtime Exeption method 25");
+		throw new InvalidTargetObjectTypeException();		
+	}
+	public void charish() throws  IOException {
+		System.out.println("Running the Runtime Exeption method 26");
+		throw new  IOException();		
+	}
+	public void mava() throws  JMException {
+		System.out.println("Running the Runtime Exeption method 27");
+		throw new  JMException();		
+	}
+	public void mane() throws KeySelectorException {
+		System.out.println("Running the Runtime Exeption method 28");
+		throw new KeySelectorException();		
+	}
+	public void atte() throws  LastOwnerException {
+		System.out.println("Running the Runtime Exeption method 29");
+		throw new  LastOwnerException();		
+	}
+	public void avva() throws LineUnavailableException {
+		System.out.println("Running the Runtime Exeption method 30");
+		throw new LineUnavailableException();		
+	}
+	public void ajji() throws  MarshalException {
+		System.out.println("Running the Runtime Exeption method 31");
+		throw new  MarshalException();		
+	}
+	public void ajja() throws MidiUnavailableException {
+		System.out.println("Running the Runtime Exeption method 32");
+		throw new MidiUnavailableException();		
+	}
+	public void comit() throws  MimeTypeParseException {
+		System.out.println("Running the Runtime Exeption method 33");
+		throw new  MimeTypeParseException();		
+	}
+	public void darshu() throws  MimeTypeParseException {
+		System.out.println("Running the Runtime Exeption method 34");
+		throw new  MimeTypeParseException();		
+	}
+	public void dham() throws NamingException {
+		System.out.println("Running the Runtime Exeption method 35");
+		throw new NamingException();		
+	}
+	public void water() throws  NoninvertibleTransformException {
+		System.out.println("Running the Runtime Exeption method 36");
+		throw new  NoninvertibleTransformException(null);		
+	}
+	public void jacky() {
+		System.out.println("Running the Error method 1");
+		throw new  AssertionError(null);
+	}
+	public void pavan() {
+		System.out.println("Running the Error method 2");
+		throw new  AWTError(null);
+	}
+	public void mahi() {
+		System.out.println("Running the Error method 3");
+		throw new  CoderMalfunctionError(null);
+	}
+	public void manoj() {
+		System.out.println("Running the Error method 4");
+		throw new  IOError(null);
+	}
+	public void ambu() {
+		System.out.println("Running the Error method 5");
+		throw new  LinkageError(null);
+	}
+	public void panda() {
+		System.out.println("Running the Error method 6");
+		throw new ServiceConfigurationError(null);
+	}
+	public void putani() {
+		System.out.println("Running the Error method 7");
+		throw new  ThreadDeath();
+	}
+	public void chintu() {
+		System.out.println("Running the Error method 8");
+		throw new  ClassCircularityError(null);
+	}
+	public void sonu() {
+		System.out.println("Running the Error method 9");
+		throw new ClassFormatError(null);
+	}
+	public void deekshu() {
+		System.out.println("Running the Error method 10");
+		throw new IncompatibleClassChangeError(null);
+	}
+	public void mamma() {
+		System.out.println("Running the Error method 11");
+		throw new  NoClassDefFoundError(null);
+	}
+	public void ammamma() {
+		System.out.println("Running the Error method 12");
+		throw new UnsatisfiedLinkError(null);
+	}
+	public void raksha() {
+		System.out.println("Running the Error method 13");
+		throw new  VerifyError(null);
+	}
+	public void sandya() {
+		System.out.println("Running the Error method 14");
+		throw new InternalError();
+	}
+	public void annaporna() {
+		System.out.println("Running the Error method 15");
+		throw new  OutOfMemoryError(null);
+	}
+	public void pillo() {
+		System.out.println("Running the Error method 16");
+		throw new  StackOverflowError(null);
+	}
+	public void bag() {
+		System.out.println("Running the Error method 17");
+		throw new  UnknownError(null);
+	}
+	public void love() {
+		System.out.println("Running the Error method 18");
+		throw new GenericSignatureFormatError(null);
+	}
+	public void sannu() {
+		System.out.println("Running the Error method 19");
+		throw new  UnsupportedClassVersionError(null);
+	}
+	public void appaji() {
+		System.out.println("Running the Error method 20");
+		throw new AbstractMethodError(null);
+	}
+	public void doddu() {
+		System.out.println("Running the Error method 21");
+		throw new  IllegalAccessError(null);
+	}
+	public void guddu() {
+		System.out.println("Running the Error method 22");
+		throw new InstantiationError(null);
+	}
+	public void gunda() {
+		System.out.println("Running the Error method 23");
+		throw new  NoSuchFieldError(null);
+	}
+	public void pushi() {
+		System.out.println("Running the Error method 24");
+		throw new UnknownError(null);
+	}
+	public void paddu() {
+		System.out.println("Running the Error method 25");
+		throw new AbstractMethodError(null);
+	}
+	public void akash() {
+		System.out.println("Running the Error method 26");
+		throw new VerifyError(null);
+	}
+	public void suju() {
+		System.out.println("Running the Error method 27");
+		throw new NoClassDefFoundError(null);
+	}
+	public void spoo() {
+		System.out.println("Running the Error method 28");
+		throw new ClassCircularityError(null);
+	}
+	public void goshal() {
+		System.out.println("Running the Error method 29");
+		throw new  InstantiationError(null);
+	}
+	public void shreya() {
+		System.out.println("Running the Error method 30");
+		throw new StackOverflowError(null);
+	}
+	
+	
+	
+	
+
+}
