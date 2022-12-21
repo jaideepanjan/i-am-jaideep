64b3ba557e857e747cfa600a9aa07f8f5e8f4464
Jaideep P <jaideepp.xworkz@gmail.com>
2022-12-21 06:58:27 +0530

    collections

---
+++ EclipsProg/collections/src/com/hp/collections/boot/CollectionRunner.java
@@ -0,0 +1,279 @@
+package com.hp.collections.boot;
+
+import java.util.Collection;
+import java.util.LinkedList;
+
+public class CollectionRunner {
+
+	public static void main(String[] args) {
+		
+	 
+		String sweet1="Champakali";
+		String sweet2="Champakali";
+		String sweet3="Champakali";
+		String sweet4="Champakali";
+		String sweet5="Champakali";
+		String sweet6="Champakali";
+		String sweet7="Champakali";
+		String sweet8="Champakali";
+		String sweet9="Champakali";
+		String sweet10="Champakali";
+		String sweet11="Champakali";
+		String sweet12="Champakali";
+		String sweet13="Champakali";
+		
+		Collection<String> collection= new LinkedList<String>();
+		
+		System.out.println(collection.add(sweet13));
+		System.out.println(collection.add(sweet12));
+		System.out.println(collection.add(sweet11));
+		System.out.println(collection.add(sweet10));
+		System.out.println(collection.add(sweet9));
+		System.out.println(collection.add(sweet8));
+		System.out.println(collection.add(sweet7));
+		System.out.println(collection.add(sweet6));
+		System.out.println(collection.add(sweet5));
+		System.out.println(collection.add(sweet4));
+		System.out.println(collection.add(sweet3));
+		System.out.println(collection.add(sweet2));
+		System.out.println(collection.add(sweet1));
+		
+		System.out.println("total Sweet numbers are = "+collection.size());
+		collection.clear();
+		
+		System.out.println("---------------------------------------------");
+		
+		String currency1="Rupies";
+		String currency2="Kaasu";
+		String currency3="Naanya";
+		String currency4="Cash";
+		String currency5="Funds";
+		String currency6="Coins";
+		String currency7="Change";
+		String currency8="Coinage";
+		String currency9="Silver";
+		String currency10="Copper";
+		String currency11="Currency";
+		String currency12="Legal Tender";
+		String currency13="Sterling";
+		String currency14="Bills";
+		String currency15="Roll";
+		String currency16="Bough";
+		String currency17="Bread";
+		String currency18="Loot";
+		String currency19="Moolah";
+		String currency20="Dibs";
+		String currency21="Gelt";
+		String currency22="Lolly";
+		String currency23="Gravy";
+		String currency24="Stuff";
+		String currency25="Brass";
+		String currency26="Dinero";
+		
+		Collection<String> collection1= new LinkedList<String>();
+		
+		System.out.println(collection1.add(currency1));
+		System.out.println(collection1.add(currency2));
+		System.out.println(collection1.add(currency3));
+		System.out.println(collection1.add(currency4));
+		System.out.println(collection1.add(currency5));
+		System.out.println(collection1.add(currency6));
+		System.out.println(collection1.add(currency7));
+		System.out.println(collection1.add(currency8));
+		System.out.println(collection1.add(currency9));
+		System.out.println(collection1.add(currency10));
+		System.out.println(collection1.add(currency11));
+		System.out.println(collection1.add(currency12));
+		System.out.println(collection1.add(currency13));
+		System.out.println(collection1.add(currency14));
+		System.out.println(collection1.add(currency15));
+		System.out.println(collection1.add(currency16));
+		System.out.println(collection1.add(currency17));
+		System.out.println(collection1.add(currency18));
+		System.out.println(collection1.add(currency19));
+		System.out.println(collection1.add(currency20));
+		System.out.println(collection1.add(currency21));
+		System.out.println(collection1.add(currency22));
+		System.out.println(collection1.add(currency23));
+		System.out.println(collection1.add(currency24));
+		System.out.println(collection1.add(currency25));
+		System.out.println(collection1.add(currency26));
+		
+		System.out.println("total Currency numbers are = "+collection1.size());
+		collection1.clear();
+		
+		System.out.println("---------------------------------------------");
+		
+		String coupon1="Free Shopping Coupons ";
+		String coupon2="Percent-off Coupns";
+		String coupon3="Free Trials";
+		String coupon4="Holiday and Seasonal Sales";
+		String coupon5="Buy 1 get 1 Free";
+		String coupon6="Abandoned Cart Coupons";
+		String coupon7="First-Time Purchase Discount";
+		String coupon8="Newsletter Subscription Discount";
+		String coupon9="Loyality Points";
+		String coupon10="Referal Promo Codes";
+		String coupon11="Custemer Milestone Discounts";
+		String coupon12="Automated Coupons";
+		
+		Collection<String> collection2= new LinkedList<String>();
+		
+		System.out.println(collection2.add(coupon1));
+		System.out.println(collection2.add(coupon2));
+		System.out.println(collection2.add(coupon3));
+		System.out.println(collection2.add(coupon4));
+		System.out.println(collection2.add(coupon5));
+		System.out.println(collection2.add(coupon6));
+		System.out.println(collection2.add(coupon7));
+		System.out.println(collection2.add(coupon8));
+		System.out.println(collection2.add(coupon9));
+		System.out.println(collection2.add(coupon10));
+		System.out.println(collection2.add(coupon11));
+		System.out.println(collection2.add(coupon12));
+		
+		System.out.println("Total Number of Coupons are  : "+collection2.size());
+		collection2.clear();
+		
+		System.out.println("-------------------------------------");
+		
+		
+		String Bike1="Puscer";
+		String Bike2="Yamaha";
+		String Bike3="Cinelli";
+		String Bike4="Salsa Cycles";
+		String Bike5="Aprilia";
+		String Bike6="Ola";
+		String Bike7="Brompton";
+		String Bike8="Tern";
+		String Bike9="Suzuki";
+		String Bike10="Huffy";
+		String Bike11="BMW";
+		String Bike12="TVS Motor";
+		String Bike13="Honda";
+		String Bike14="Schwinn";
+		String Bike15="Harley-Davidson";
+		String Bike16="Trek";
+		String Bike17="Bianchi";
+		String Bike18="Cannondale";
+		
+		Collection<String> collection3=new LinkedList<String>();
+		
+		System.out.println(collection3.add(Bike1));
+		System.out.println(collection3.add(Bike2));
+		System.out.println(collection3.add(Bike3));
+		System.out.println(collection3.add(Bike4));
+		System.out.println(collection3.add(Bike5));
+		System.out.println(collection3.add(Bike6));
+		System.out.println(collection3.add(Bike7));
+		System.out.println(collection3.add(Bike8));
+		System.out.println(collection3.add(Bike9));
+		System.out.println(collection3.add(Bike10));
+		System.out.println(collection3.add(Bike11));
+		System.out.println(collection3.add(Bike12));
+		System.out.println(collection3.add(Bike13));
+		System.out.println(collection3.add(Bike14));
+		System.out.println(collection3.add(Bike15));
+		System.out.println(collection3.add(Bike16));
+		System.out.println(collection3.add(Bike17));
+		System.out.println(collection3.add(Bike18));
+		
+		 System.out.println("Total number of Bikes are : "+collection3.size());
+		 collection3.clear();
+		System.out.println("--------------------------------------");
+		
+		
+		String mall1="Gopalan Mall";
+		String mall2="LuLu Mall";
+		String mall3="Orion Mall";
+		String mall4="DFL Mall";
+		String mall5="Mantri Square Mall";
+		String mall6="GT Mall";
+		String mall7="Infinit MAll";
+		String mall8="AJ infinit Mall";
+		String mall9="Nexaus Sujana Mall";
+		String mall10="Gopalan Mall";
+		String mall11="Inorbit Mall";
+		String mall12="MGF Metropolis Mall";
+		String mall13="Sarath City Capital Mall";
+		String mall14="Ambience Mall";
+		String mall15="World Trade Park Mall";
+		String mall16="Hilite Mall";
+		String mall17="Nexus Ahmedabad Mall";
+		String mall18="Amanora Mall";
+		String mall19="Ambience Mall";
+		String mall20="R City Mall";
+		String mall21="Phoenix Marketcity";
+		String mall22="Elante Mall";
+		String mall23="Magneto the mall";
+		String mall24="South City Mall";
+		String mall25="Formerly North Country Mall";
+		String mall26="Formerly Trilium Mall";
+		String mall27="Viviana Mall";
+		String mall28="City Centre Mall";
+		String mall29="Z Square Mall";
+		String mall30="Gaur City Mall";
+		String mall31="Inorbit Mall";
+		String mall32="Ansal Plaza, Greater Noida Mall";
+		String mall33="Mani Square Mall";
+		String mall34="Quest Mall";
+		String mall35="The Nexus Vijaya Mall";
+		String mall36="Vegas Mall";
+		String mall37="Parsvnath Mall";
+		String mall38="Prozone Mall";
+		String mall39="Royal Meenakshi Mall";
+		String mall40="Moments Mall";
+		
+		Collection<String> collection4=new LinkedList<String>();
+		
+		System.out.println(collection4.add(mall1));
+		System.out.println(collection4.add(mall2));
+		System.out.println(collection4.add(mall3));
+		System.out.println(collection4.add(mall4));
+		System.out.println(collection4.add(mall5));
+		System.out.println(collection4.add(mall6));
+		System.out.println(collection4.add(mall7));
+		System.out.println(collection4.add(mall8));
+		System.out.println(collection4.add(mall9));
+		System.out.println(collection4.add(mall10));
+		System.out.println(collection4.add(mall11));
+		System.out.println(collection4.add(mall12));
+		System.out.println(collection4.add(mall13));
+		System.out.println(collection4.add(mall14));
+		System.out.println(collection4.add(mall15));
+		System.out.println(collection4.add(mall16));
+		System.out.println(collection4.add(mall17));
+		System.out.println(collection4.add(mall18));
+		System.out.println(collection4.add(mall19));
+		System.out.println(collection4.add(mall20));
+		System.out.println(collection4.add(mall21));
+		System.out.println(collection4.add(mall22));
+		System.out.println(collection4.add(mall23));
+		System.out.println(collection4.add(mall24));
+		System.out.println(collection4.add(mall25));
+		System.out.println(collection4.add(mall26));
+		System.out.println(collection4.add(mall27));
+		System.out.println(collection4.add(mall28));
+		System.out.println(collection4.add(mall29));
+		System.out.println(collection4.add(mall30));
+		System.out.println(collection4.add(mall31));
+		System.out.println(collection4.add(mall32));
+		System.out.println(collection4.add(mall33));
+		System.out.println(collection4.add(mall34));
+		System.out.println(collection4.add(mall35));
+		System.out.println(collection4.add(mall36));
+		System.out.println(collection4.add(mall37));
+		System.out.println(collection4.add(mall38));
+		System.out.println(collection4.add(mall39));
+		System.out.println(collection4.add(mall40));
+		
+		System.out.println("Total number of Mall are : "+collection4.size());
+		collection4.clear();
+		System.out.println("~~~~~~~~~~Completed~~~~~~~~~~");		
+
+	}
+
+	
+	}
+
+
